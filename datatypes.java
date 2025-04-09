3dc4fc452fdcd963e3e9b72cb2cd21a4cda8bf6c
ShreyaRajalabndi <rajalabanadishreya2005@gmail.com>
2025-04-09 16:10:29 +0530

    data types

---
+++ datatypes.java
@@ -0,0 +1,22 @@
+public class datatypes {
+    public static void main(String[] args){
+        byte a=100;
+        short b=1000;
+        int c=10000;
+        long d=1000000000;
+        float e=5.75f;
+        double f=19.99;
+        char g='A';
+        boolean h=true;
+        String i="Hello Github!!";
+        System.out.println("Byte: "+a);
+        System.out.println("Short: "+b);
+        System.out.println("Integer: "+c);
+        System.out.println("Long integer: "+d);
+        System.out.println("Float: "+e);
+        System.out.println("Double: "+f);
+        System.out.println("Character: "+g);
+        System.out.println("Boolean: "+h);
+        System.out.println("String: "+i);
+    }
+}
